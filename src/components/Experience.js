e5c7b1ec4beda43be075e5cd218a6b40b8bad978
DVSHKMR <ashudevesh654@gmail.com>
2022-09-07 13:50:00 +0530

    Initial Commit

---
+++ src/components/Experience.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Img1 from '../assets/img/exp-img1.png'
+import Img2 from '../assets/img/exp-img2.png'
+
+const Experience = () => {
+  return <section className='mb-12 lg:mb-24 lg:mt-6'>
+    <div className="container mx-auto">
+      <div className='flex flex-col min-h-[480px] lg:space-x-20 lg:flex-row'>
+        <div className='flex-1 flex space-x-6 items-center lg:space-x-12'>
+          <div className='self-start' data-aos='fade-down' data-aos-offset='400'>
+            <img src={Img1} alt="" />
+          </div>
+          <div className='self-end' data-aos='fade-up' data-aos-offset=''>
+            <img src={Img2} alt="" />
+          </div>
+        </div>
+
+        <div className='flex-1 flex flex-col items-start justify-center mt-6 lg:mt-0' data-aos='fade-left' data-aos-offset='400'>
+          <h2 className='text-3xl font-bold mb-6'>New Experience in Playing Games</h2>
+          <p className='font-secondary mb-6'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus iste nostrum dignissimos expedita totam, optio quaerat culpa nisi sint at.</p>
+          <button className='btn'>Get it now</button>
+        </div>
+      </div>
+    </div>
+  </section>
+};
+
+export default Experience;
